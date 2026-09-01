334fb513f1ef7689fdc6b957b44f8fc1bc91d0eb
lyzasfs <Phryl.amper@gmail.com>
2026-09-01 18:46:17 +0800

    Fix live backend API URL

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://your-render-backend-1.onrender.com/api',
+  baseURL: 'https://archportal-backend-1.onrender.com/api',
   withCredentials: false,
   headers: {
     'Content-Type': 'application/json',
